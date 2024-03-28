0334d4a0b9aed0b0b4d63364f2e11c39bc2ddaa1
Sergio Clemente <sergiotucano@yahoo.com.br>
2024-03-28 17:00:58 -0300

    retirar network info plus

--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -1,26 +1,24 @@
 import 'dart:io';
-import 'package:network_info_plus/network_info_plus.dart';
-import 'package:pos_printer_manager/helpers/network_analyzer.dart';
-import 'package:pos_printer_manager/pos_printer_manager.dart';
 
 class NetworkService {}
 
 Future<List<String>> findNetworkPrinter({int port = 9100}) async {
-  final _info = NetworkInfo();
-  String ip = await (_info.getWifiIP())??'';
-  if (ip.isEmpty == true) {
-    ip = (await getAddresses()).first;
-  }
-  PosPrinterManager.logger.info("ip: $ip");
-  final String subnet = ip.substring(0, ip.lastIndexOf('.'));
-  PosPrinterManager.logger.info("subnet: $subnet");
-  PosPrinterManager.logger.info("port: $port");
-
-  final stream = NetworkAnalyzer.discover2(subnet, port);
-  var results = await stream.toList();
-  return [
-    ...results.where((entry) => entry.exists).toList().map((e) => e.ip).toList()
-  ];
+  return [];
+  // final _info = NetworkInfo();
+  // String ip = await (_info.getWifiIP())??'';
+  // if (ip.isEmpty == true) {
+  //   ip = (await getAddresses()).first;
+  // }
+  // PosPrinterManager.logger.info("ip: $ip");
+  // final String subnet = ip.substring(0, ip.lastIndexOf('.'));
+  // PosPrinterManager.logger.info("subnet: $subnet");
+  // PosPrinterManager.logger.info("port: $port");
+  //
+  // final stream = NetworkAnalyzer.discover2(subnet, port);
+  // var results = await stream.toList();
+  // return [
+  //   ...results.where((entry) => entry.exists).toList().map((e) => e.ip).toList()
+  // ];
 }
 
 Future<List<String>> getAddresses() async {
